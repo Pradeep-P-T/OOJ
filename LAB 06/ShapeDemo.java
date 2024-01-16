b8f4fe8ecb6434a961c7c7d4615b06ef7783b56f
Pradeep-P-T <153193804+Pradeep-P-T@users.noreply.github.com>
2024-01-16 22:53:59 +0530

    Update ShapeDemo.java

--- LAB 06/ShapeDemo.java
+++ LAB 06/ShapeDemo.java
@@ -1,27 +1,16 @@
 abstract class Shape {
-    // Abstract method to calculate the area
     public abstract double calculateArea();
-
-    // Abstract method to calculate the perimeter
     public abstract double calculatePerimeter();
 }
 
 class Circle extends Shape {
     private double radius;
-
-    // Constructor for Circle
     public Circle(double radius) {
         this.radius = radius;
     }
-
-    // Implementing calculateArea() method for Circle
-    @Override
     public double calculateArea() {
         return Math.PI * radius * radius;
     }
-
-    // Implementing calculatePerimeter() method for Circle
-    @Override
     public double calculatePerimeter() {
         return 2 * Math.PI * radius;
     }
@@ -31,23 +20,15 @@ class Triangle extends Shape {
     private double side1;
     private double side2;
     private double side3;
-
-    // Constructor for Triangle
     public Triangle(double side1, double side2, double side3) {
         this.side1 = side1;
         this.side2 = side2;
         this.side3 = side3;
     }
-
-    // Implementing calculateArea() method for Triangle using Heron's formula
-    @Override
     public double calculateArea() {
         double s = (side1 + side2 + side3) / 2;
         return Math.sqrt(s * (s - side1) * (s - side2) * (s - side3));
     }
-
-    // Implementing calculatePerimeter() method for Triangle
-    @Override
     public double calculatePerimeter() {
         return side1 + side2 + side3;
     }
@@ -55,14 +36,9 @@ class Triangle extends Shape {
 
 public class ShapeDemo {
     public static void main(String[] args) {
-        // Creating instances of Circle and Triangle
         Circle circle = new Circle(5.0);
         Triangle triangle = new Triangle(3.0, 4.0, 5.0);
-
-        // Calculating and displaying area and perimeter for Circle
         System.out.println("Circle - Area: " + circle.calculateArea() + ", Perimeter: " + circle.calculatePerimeter());
-
-        // Calculating and displaying area and perimeter for Triangle
         System.out.println("Triangle - Area: " + triangle.calculateArea() + ", Perimeter: " + triangle.calculatePerimeter());
     }
 }
